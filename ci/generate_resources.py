95514b700213573a2690d4a9a239985b544abd71
pipelines-as-code <project_73447720_bot_f3dd781081811ff0cc5e7722b5756454@noreply.gitlab.com>
2026-05-27 00:22:14 +0000

    Update quay.io/hummingbird-community/syft:latest Docker digest to 2b2612e
    Signed-off-by: pipelines-as-code <project_73447720_bot_f3dd781081811ff0cc5e7722b5756454@noreply.gitlab.com>

--- ci/generate_resources.py
+++ ci/generate_resources.py
@@ -44,7 +44,7 @@ def expand_task_run_specs(specs: list) -> list:
 PIPELINE_BUNDLE = "quay.io/hummingbird-ci/rpmbuild-pipeline:latest@sha256:c2a5cff5145786d931222644c3baa9814c57f8e3e249af750bfc0352fe4c8d31"
 
 # renovate: datasource=docker depName=quay.io/hummingbird-community/syft
-SYFT_IMAGE = "quay.io/hummingbird-community/syft:latest-builder@sha256:c4048cb63f65444cbbabef58076dafb954342ee02739c051c37ead38389e08ec"
+SYFT_IMAGE = "quay.io/hummingbird-community/syft:latest-builder@sha256:2b2612eb8dfb505f3d1e11ef6408830749d3a68e44673d6fd0ef3d3acfb6ec71"
 
 def load_yaml_file(path: Path) -> dict | None:
     """Load a YAML file, returning None if it doesn't exist."""
